bb726129eb5503ddf498be14a49b80df987b8823
matsovevada <55790581+matsovevada@users.noreply.github.com>
2021-04-21 22:26:50 +0200

    fixed prototype navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
+import Header from "./components/Header.js"
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Header fixed='top'/>
     </div>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import * as RBS from 'react-bootstrap'
+
+
+const Header = () => {
+    return (
+        <RBS.Navbar bg="light" expand="md">
+            <RBS.Navbar.Brand href="">Webshop</RBS.Navbar.Brand>
+            <RBS.Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <RBS.Navbar.Collapse id="basic-navbar-nav">
+            <RBS.Nav className="mr-auto">
+                <RBS.Nav.Link href="">Home</RBS.Nav.Link>
+                <RBS.NavDropdown title="Products" id="basic-nav-dropdown">
+                    <RBS.NavDropdown.Item href="">Food</RBS.NavDropdown.Item>
+                    <RBS.NavDropdown.Item href="">Tech</RBS.NavDropdown.Item>
+                    <RBS.NavDropdown.Item href="">Clothes</RBS.NavDropdown.Item>
+                    <RBS.NavDropdown.Item href="">Furniture</RBS.NavDropdown.Item>
+                </RBS.NavDropdown>
+                <RBS.Nav.Link href="">Contact us</RBS.Nav.Link>
+                </RBS.Nav>
+                <RBS.Form inline>
+                    <RBS.FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <RBS.Button variant="outline-success">Search</RBS.Button>
+                </RBS.Form>
+            </RBS.Navbar.Collapse>
+        </RBS.Navbar>
+    )
+}
+
+export default Header
