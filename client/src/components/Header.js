7ec06be4cd06fe210f91392527455bef164e023c
matsovevada <55790581+matsovevada@users.noreply.github.com>
2021-04-22 14:23:06 +0200

    added react router and fixed navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,22 +6,28 @@ import * as RBS from 'react-bootstrap'
 const Header = () => {
     return (
         <RBS.Navbar bg="light" expand="md">
-            <RBS.Navbar.Brand href="">Webshop</RBS.Navbar.Brand>
+            <RBS.Navbar brand={require('../pics/NideoVova.png')}href="/"/>
             <RBS.Navbar.Toggle aria-controls="basic-navbar-nav" />
             <RBS.Navbar.Collapse id="basic-navbar-nav">
             <RBS.Nav className="mr-auto">
-                <RBS.Nav.Link href="">Home</RBS.Nav.Link>
-                <RBS.NavDropdown title="Products" id="basic-nav-dropdown">
-                    <RBS.NavDropdown.Item href="">Food</RBS.NavDropdown.Item>
-                    <RBS.NavDropdown.Item href="">Tech</RBS.NavDropdown.Item>
-                    <RBS.NavDropdown.Item href="">Clothes</RBS.NavDropdown.Item>
-                    <RBS.NavDropdown.Item href="">Furniture</RBS.NavDropdown.Item>
+                <RBS.Nav.Link href="/">Home</RBS.Nav.Link>
+                <RBS.NavDropdown title="Genre" id="basic-nav-dropdown">
+                    <RBS.NavDropdown.Item href="/food">Action</RBS.NavDropdown.Item>
+                    <RBS.NavDropdown.Item href="/tech">Comedy</RBS.NavDropdown.Item>
+                    <RBS.NavDropdown.Item href="/clothes">Drama</RBS.NavDropdown.Item>
+                    <RBS.NavDropdown.Item href="/furniture">Rom-Com</RBS.NavDropdown.Item>
                 </RBS.NavDropdown>
-                <RBS.Nav.Link href="">Contact us</RBS.Nav.Link>
+                <RBS.Nav.Link href="/contact-us">Contact us</RBS.Nav.Link>
                 </RBS.Nav>
                 <RBS.Form inline>
                     <RBS.FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <RBS.Button variant="outline-success">Search</RBS.Button>
+                    <RBS.Button variant="outline-success" id='searchbar'>Search</RBS.Button>
+                </RBS.Form>
+                <RBS.Navbar.Brand href="/cart">Cart: </RBS.Navbar.Brand>
+                <RBS.Form inline>
+                    <RBS.FormControl type="username" placeholder="Username" className="mr-sm-2" />
+                    <RBS.FormControl type="password" placeholder="Password" className="mr-sm-2" />
+                    <RBS.Button variant="outline-success" id='searchbar' type='submit'>Login</RBS.Button>
                 </RBS.Form>
             </RBS.Navbar.Collapse>
         </RBS.Navbar>
