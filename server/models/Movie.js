a94ff0fa02a24a5c75d8b4f997baac0e3a5857a3
Zakaria Karlsen Tawfiq <zaktaw@gmail.com>
2021-04-23 09:12:37 +0200

    Update MovieSchema.js

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 const MovieSchema = mongoose.Schema({
-    title: String
+    title: String,
+    shortDescription: String,
+    longDescription: Object,
+    price: Number,
+    genre: String,
+    creationDate: Date.now,
+    pictureLinks: [String]
 });
 
 module.exports = mongoose.model('Movie', MovieSchema)
