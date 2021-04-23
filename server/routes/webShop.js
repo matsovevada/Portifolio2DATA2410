4d97d2436b60a27f9c092d1ee1683329c41cf0c3
Zakaria Karlsen Tawfiq <zaktaw@gmail.com>
2021-04-23 11:20:49 +0200

    Updated get function for getting movies by title and by genre

--- server/routes/webShop.js
+++ server/routes/webShop.js
@@ -3,23 +3,14 @@ const Movie = require('../models/Movie')
 
 const router = express.Router();
 
-// Get all movies
-router.get('/', (req, res) => {
-    Movie.find()
-        .then((data) => {
-            res.status(200).json(data)
-        })
-        .catch((err) => {
-            res.status(400).json({
-                "Status": 400,
-                "Message": "Couldn't find movies"
-            })
-        });
-})
+router.get('/movies', (req, res) => {
 
-// Get all movies containing given search param
-router.get('/:title', (req, res) => {
-    Movie.find({title: req.params.title})
+    console.log(req.body.title)
+    console.log(req.body.genre)
+
+    // get all movies matching search param
+    if (req.body.title) {
+        Movie.find({title: req.body.title})
         .then((data) => {
             res.status(200).json(data)
         })
@@ -29,6 +20,35 @@ router.get('/:title', (req, res) => {
                 "Message": "Couldn't find movies"
             })
         });
+    }
+
+    // get all movies by genre
+    else if (req.body.genre) {
+        Movie.find({genre: req.body.genre})
+        .then((data) => {
+            res.status(200).json(data)
+        })
+        .catch((err) => {
+            res.status(404).json({
+                "Status": 404,
+                "Message": "Couldn't find movies"
+            })
+        });
+    }
+
+    // get all movies
+    else {
+        Movie.find()
+        .then((data) => {
+            res.status(200).json(data)
+        })
+        .catch((err) => {
+            res.status(404).json({
+                "Status": 404,
+                "Message": "Couldn't find movies"
+            })
+        });
+    }
 })
 
 // Get all movies by genre
@@ -38,6 +58,27 @@ router.get('/', (req, res) => {
         .then((data) => {
             res.json(data)
         });
+})
+
+router.post('/movie', (req, res) => {
+    const movie = new Movie({
+        title: req.body.title,
+        shortDescription: req.body.shortDescription,
+        longDescription: req.body.longDescription,
+        price: req.body.price,
+        genre: req.body.genre,
+    }) 
+
+    movie.save()
+        .then((data) => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(400).json({
+                "Status": 400,
+                "Message": "Couldn't add movie"
+            })
+        })
 })
 
 module.exports = router;
