623ddf81acfbab6cd53d4946f3ee7fbd55b9bfc9
Peter <peter.stjernsund@gmail.com>
2021-04-26 14:42:41 +0200

    Get movie?

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,40 @@
 import React from 'react'
 
+export default function getMovies() {
+
 const home = () => {
+
     return (
         <div>
-            <h1>Hello HOME!</h1>
-        </div>
+            <p>movies!!!</p>
+        </div>    
     )
 }
 
-export default home
+const requestOptions = {
+    method: 'GET',
+    body: formData
+
+};
+fetch(url, requestOptions)
+    .then(async response => {
+        const isJson = response.headers.get('content-type')?.includes('application/json');
+        const data = isJson && await response.json();
+
+        // check for error response
+        if (!response.ok) {
+            // get error message from body or default to response status
+            const error = (data && data.message) || response.status;
+            return Promise.reject(error);
+        }
+
+    })
+    .catch(error => {
+        console.error('There was an error!', error);
+    });
+
+
+return (
+
+)
+}
