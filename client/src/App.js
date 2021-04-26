b01009811ec39542d7b69b448d8b5638c339b454
Zakaria Karlsen Tawfiq <zaktaw@gmail.com>
2021-04-26 13:56:01 +0200

    Added component for adding new movies

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Login from './components/Login.js'
 import Home from './components/Home.js'
 import AboutUs from './components/About.js'
 import Register from './components/Register.js'
+import FormMovie from './components/FormMovie.js'
 import {Route} from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path='/about-us' component={AboutUs}/>
       <Route path='/login' component={Login}/>
       <Route path='/register' component={Register}/>
+      <Route path='/formMovie' component={FormMovie}/>
     </div>
   );
 }
