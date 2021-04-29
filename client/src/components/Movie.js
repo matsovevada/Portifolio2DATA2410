d6d5b3eb823c9b417682fd4f84aa05e3840473ff
Peter <peter.stjernsund@gmail.com>
2021-04-29 15:40:26 +0200

    Added conversion from buffer binary to base64 to support displaying of images from db

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -11,7 +11,16 @@ const Movie = ({movie}) => {
     else setShowExtendedInformation(true)
 
     // showExtendedInformation ? setShowExtendedInformation(false) : setShowExtendedInformation(true)
+
 }
+
+//Credit: https://stackoverflow.com/questions/9267899/arraybuffer-to-base64-encoded-string
+const arrayBufferToBase64 = (buffer) => {
+    let binary = '';
+    let bytes = [].slice.call(new Uint8Array(buffer));
+    bytes.forEach((b) => binary += String.fromCharCode(b));
+    return window.btoa(binary);
+};
 
 
     return (
@@ -19,6 +28,8 @@ const Movie = ({movie}) => {
             <h3>Title: {movie.title}</h3>
             <h3>Description: {movie.description}</h3>
             <h3>Price: {movie.price}</h3>
+            { movie.img ? <img src={`data:image/png;base64,${arrayBufferToBase64(movie.img.data.data)}`}/> : null} 
+        
             { showExtendedInformation ? <h3>Genre: </h3> : null }
             <button onClick={() => toggleShowExtendedInformation()}>Show more</button>
         </div>
