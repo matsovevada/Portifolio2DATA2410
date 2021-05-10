a678738f449bba59cca735e2f56b1422b6b711be
Zakaria Karlsen Tawfiq <zaktaw@gmail.com>
2021-05-10 10:56:50 +0200

    Added credentials: include to register api call

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -40,6 +40,7 @@ export default function Register() {
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
+            credentials: "include",
             body: JSON.stringify(data)
         };
         fetch('http://localhost:8080/user', requestOptions)
