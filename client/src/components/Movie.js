5885ad7fb9a4cca540c7b8f3ed9460e0378834c6
Peter <peter.stjernsund@gmail.com>
2021-05-11 15:03:13 +0200

    Small fix 2.....

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,4 +1,5 @@
 import React, { useState} from 'react'
+import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import * as RBS from 'react-bootstrap'
 import ModalPop from './ModalPop.js'
@@ -44,11 +45,9 @@ const Movie = ({movie, updateCart, admin_deleteMovie, admin_editMovie, user}) =>
                 {(user != null && user.isAdmin) && <RBS.Button variant='danger' onClick={() => admin_deleteMovie(movie)}>Delete</RBS.Button>}
                 {(user != null && user.isAdmin) && <AdminPop admin_editMovie={admin_editMovie} _id={movie._id} title={movie.title} shortDescription={movie.shortDescription} longDescription={movie.longDescription} genre={movie.genre} price={movie.price} img={movie.img ? <img src={`data:image/png;base64,${arrayBufferToBase64(movie.img.data.data)}`}/> : null}/>}
                 
-                {/* Toggle pop-up for more info */}
+                {/* Toggle pop-up for more info. Display default image if not present in DB */}
                 { showExtendedInformation ? <h3>Genre: {movie.genre}</h3> : null }
                 <ModalPop title={movie.title} longDescription={movie.longDescription} genre={movie.genre} price={movie.price}
-                
-                {/* Display default image of not present in DB */}
                 img={ movie.img ? <img src={`data:image/png;base64,${arrayBufferToBase64(movie.img.data.data)}`} className='MovieViewPicture'/> : <img src={DefaultPicture} className='MovieViewPicture'/>} />
                 
                 {/* Display "add to cart" to all users except admin */}
