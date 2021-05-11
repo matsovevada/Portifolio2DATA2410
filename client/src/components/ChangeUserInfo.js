05eb43f31a70cb81034b473116e1f9e7296a0c61
Zakaria Karlsen Tawfiq <zaktaw@gmail.com>
2021-05-11 14:25:18 +0200

    Added error handling for check user info

--- client/src/components/ChangeUserInfo.js
+++ client/src/components/ChangeUserInfo.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Input from './Input'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
@@ -11,12 +11,35 @@ export default function ChangeUserInfo({user}){
     const [zipcode, setZipcode] = useState('')
     const [city, setCity] = useState('')
 
+    const [showErrorFirstname, setShowErrorFirstname] = useState(false)
+    const [showErrorLastname, setShowErrorLastname] = useState(false)
+    const [showErrorAddress, setShowErrorAddress] = useState(false)
+    const [showErrorZipcode, setShowErrorZipcode] = useState(false)
+    const [showErrorCity, setShowErrorCity] = useState(false)
+
+    useEffect(() => {
+        if (firstname.length < 2) setShowErrorFirstname(true)
+        else setShowErrorFirstname(false)
+
+        if (lastname.length < 2) setShowErrorLastname(true)
+        else setShowErrorLastname(false)
+
+        if (address.length < 6) setShowErrorAddress(true)
+        else setShowErrorAddress(false)
+
+        if (zipcode.length < 4) setShowErrorZipcode(true)
+        else setShowErrorZipcode(false)
+
+        if (city.length < 2) setShowErrorCity(true)
+        else setShowErrorCity(false)
+      })
+
     function checkLength() {
-        return firstname.length > 2 
-        && lastname.length > 2
+        return firstname.length > 1 
+        && lastname.length > 1
         && address.length > 5
         && zipcode.length > 3
-        && city.length > 2;
+        && city.length > 1;
     }
 
     function editUser(e){
@@ -59,11 +82,11 @@ export default function ChangeUserInfo({user}){
         <div className='ChangeUserInfo'>
             <Form>
                 <h1>Change user info:</h1>
-                <Input title={user != null && user.firstname} value={firstname} type='firstname' set={setFirstname} placeholder={'new firstname'}/>
-                <Input title={user != null && user.lastname} value={lastname} type='lastname' set={setLastname} placeholder={'new lastname'}/>
-                <Input title={user != null && user.address} value={address} type='address' set={setAddress} placeholder={'new address'}/>
-                <Input title={user != null && user.zipcode} value={zipcode} type='zipcode' set={setZipcode} placeholder={'new zipcode'}/>
-                <Input title={user != null && user.city} value={city} type='city' set={setCity} placeholder={'new city'}/>
+                <Input title={user != null && user.firstname} value={firstname} type='firstname' set={setFirstname} placeholder={'new firstname'} errorText={'First name must have at least two characters'} setShowError={setShowErrorFirstname} showError={showErrorFirstname}/>
+                <Input title={user != null && user.lastname} value={lastname} type='lastname' set={setLastname} placeholder={'new lastname'} errorText={'Last name must have at least two characters'} setShowError={setShowErrorLastname} showError={showErrorLastname}/>
+                <Input title={user != null && user.address} value={address} type='address' set={setAddress} placeholder={'new address'} errorText={'Address must have at least six characters'} setShowError={setShowErrorAddress} showError={showErrorAddress}/>
+                <Input title={user != null && user.zipcode} value={zipcode} type='zipcode' set={setZipcode} placeholder={'new zipcode'} errorText={'Zipcode name must have at least four characters'} setShowError={setShowErrorZipcode} showError={showErrorZipcode}/>
+                <Input title={user != null && user.city} value={city} type='city' set={setCity} placeholder={'new city'} errorText={'City must have at least two characters'} setShowError={setShowErrorCity} showError={showErrorCity}/>
                 <Button variant='danger' disabled={!checkLength()} onClick={editUser}>Change</Button>
             </Form>
         </div>
