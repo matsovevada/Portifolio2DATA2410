caeb954ed098477e8a82a51428f12099061b98fb
Peter <peter.stjernsund@gmail.com>
2021-05-11 14:56:36 +0200

    Comments in movie.js

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -41,12 +41,16 @@ const Movie = ({movie, updateCart, admin_deleteMovie, admin_editMovie, user}) =>
                 <h3 className='MovieViewGenre'>Genre: {movie.genre}</h3>
                 <h3 className='MovieViewPrice'>Price: {movie.price}</h3>
                 
-                {/* Adminfunction!! */}
+                {/* Adminfunctions */}
                 {(user != null && user.isAdmin) && <RBS.Button variant='danger' onClick={() => admin_deleteMovie(movie)}>Delete</RBS.Button>}
                 {(user != null && user.isAdmin) && <AdminPop admin_editMovie={admin_editMovie} _id={movie._id} title={movie.title} shortDescription={movie.shortDescription} longDescription={movie.longDescription} genre={movie.genre} price={movie.price} img={movie.img ? <img src={`data:image/png;base64,${arrayBufferToBase64(movie.img.data.data)}`}/> : null}/>}
+                
+                {/* Toggle pop-up for more info */}
                 { showExtendedInformation ? <h3>Genre: {movie.genre}</h3> : null }
                 <ModalPop title={movie.title} longDescription={movie.longDescription} genre={movie.genre} price={movie.price} 
                 img={ movie.img ? <img src={`data:image/png;base64,${arrayBufferToBase64(movie.img.data.data)}`} className='MovieViewPicture'/> : <img src={DefaultPicture} className='MovieViewPicture'/>} />
+                
+                {/* Display "add to cart" to all users except admin */}
                 {((user == null) || (user != null && user.isAdmin == false)) && <RBS.Button variant='secondary' onClick={() => updateCart(movie)}>Add to cart</RBS.Button>}
             </div>
         </div>
