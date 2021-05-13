fdd6c402fbb3afaedbbd3d26213057d1af874480
Zakaria Karlsen Tawfiq <zaktaw@gmail.com>
2021-05-13 09:36:53 +0200

    Added comments and removed unused functions in backend

--- server/app.js
+++ server/app.js
@@ -3,23 +3,10 @@ const mongoose = require('mongoose');
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
-const https = require('https')
-const fs = require('fs')
-
-//const options = {
-//  key: fs.readFileSync("/srv/www/keys/my-site-key.pem"),
-//  cert: fs.readFileSync("/srv/www/keys/chain.pem")
-// };
-
+// database
 const path = require('path')
-const Movie = require('./models/Movie');
-const User = require('./models/User')
 mongoose.connect('mongodb://localhost:27017/WebShop', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
-
-const webShop = require('./routes/webShop')
-const user = require('./routes/user')
-const admin = require('./routes/admin')
 
 mongoose.set('useFindAndModify', false);
 
@@ -31,35 +18,21 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+// middleware
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// middleware
-// app.use(cors())
 app.use(cors({origin:true, credentials: true}));
 
+// routes
+const webShop = require('./routes/webShop')
+const user = require('./routes/user')
+const admin = require('./routes/admin')
 
 app.use('/webShop', webShop)
 app.use('/user', user)
 app.use('/admin', admin)
-
-app.use('/user', function(res, req, next) {
-  console.log('user')
-  next()
-})
-
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-
-app.get("/test", (req, res) => {
-  res.sendFile(path.join(__dirname, 'test.html'))
-});
   
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
-
-//https.createServer(options, app).listen(8000);
-
--- server/models/User.js
+++ server/models/User.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-    _id: String,
-    email: String,
-    firstname: String,
-    lastname: String,
-    address: String,
-    zipcode: String,
-    city: String,
-    orderHistory: {type : [Object], default : [] },
-    shoppingCart: {type : [Object], default : [] },
-    isAdmin: {type: Boolean, default: false}
+    _id: {type: String, required: true},
+    email: {type: String, required: true},
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+    address: {type: String, required: true},
+    zipcode: {type: String, required: true},
+    city: {type: String, required: true},
+    orderHistory: { type : [Object], default : [] },
+    shoppingCart: { type : [Object], default : [] },
+    isAdmin: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('User', UserSchema)
